2e08f5d5c1093b5239a12399ce60bd3f509a46c6
Emmanuel <emmanuelatuahene16@gmail.com>
2020-11-20 20:22:14 -0500

    added testing h1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -30,6 +30,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <h1>ravenous</h1>
+        <h1>testing</h1>
         <SearchBar />
         <BusinessList businesses={businesses} />
       </div>
